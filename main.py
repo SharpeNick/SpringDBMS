5b89c0b2bb84279e30be77945bbc23017c9f9415
Nick <nws2q@mtmail.mtsu.edu>
2024-03-20 20:48:47 -0500

    Added more details

--- main.py
+++ main.py
@@ -7,8 +7,28 @@ def hello_world():
     return render_template('index.html')
 
 @app.route('/about')
-def hello():
+def about():
     return render_template('about.html')
+
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
+
+@app.route('/pay')
+def pay():
+    return render_template('pay.html')
+
+@app.route('/appt')
+def appt():
+    return render_template('appt.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
