36685e4dc104ecc978359a2f46c802d111431ab3
Kshetkar1 <114525816+Kshetkar1@users.noreply.github.com>
2024-03-21 12:54:14 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,9 +15,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class PatientInformation(db.Model):
-    __tablename__ = 'Patient_Information'
-    Patient_ID = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'New_Patient_Information'
+    Patient_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    FName = db.Column(db.String(50))
+    LName = db.Column(db.String(50))
     Height = db.Column(db.Integer)
     Weight = db.Column(db.Integer)
     Age = db.Column(db.Integer)
@@ -27,72 +30,117 @@ class PatientInformation(db.Model):
     Symptoms = db.Column(db.Text)
     Reason_for_visit = db.Column(db.Text)
     Last_Reason_for_visit = db.Column(db.Text)
-    Doctor_in_care = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'))
-    
-    # Assuming a one-to-one relationship with Nurse_Form
-    nurse_form = db.relationship('NurseForm', backref='patient_information', uselist=False)
-
-class NurseForm(db.Model):
-    __tablename__ = 'Nurse_Form'
-    Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient_Information.Patient_ID'), primary_key=True)
-    Height = db.Column(db.Integer)
-    Weight = db.Column(db.Integer)
-    Age = db.Column(db.Integer)
-    Gender = db.Column(db.String(50))
-    Blood_Pressure = db.Column(db.Integer)
-    Medication = db.Column(db.Text)
-    Symptoms = db.Column(db.Text)
-    Doctor_in_care = db.Column(db.Integer)
-
-class Insurance(db.Model):
-    __tablename__ = 'Insurance'
-    Insurance_Name = db.Column(db.Integer, primary_key=True)
-    Policy_Number = db.Column(db.Integer)
-    Covered = db.Column(db.Boolean)
-    Deductible = db.Column(db.Integer)
-    
-    # Assuming a one-to-many relationship with Patient
-    patients = db.relationship('Patient', backref='insurance')
-
-class Patient(db.Model):
-    __tablename__ = 'Patient'
-    Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient_Information.Patient_ID'), primary_key=True)
-    FName = db.Column(db.String(50))
-    LName = db.Column(db.String(50))
-    Insurance_Name = db.Column(db.Integer, db.ForeignKey('Insurance.Insurance_Name'))
-    Covered = db.Column(db.Boolean)
+    Doctor_in_care = db.Column(db.Integer, db.ForeignKey('employee.Employee_ID'))
+    Insurance_Name = db.Column(db.Integer, db.ForeignKey('insurance.Insurance_Name'))
     Phone = db.Column(db.Integer)
     Address = db.Column(db.String(255))
     EContact_Name = db.Column(db.String(50))
     EContact_Phone = db.Column(db.Integer)
+    Email = db.Column(db.String(255), unique=True)
 
-class Employee(db.Model):
-    __tablename__ = 'Employee'
-    Employee_ID = db.Column(db.Integer, primary_key=True)
-    Position = db.Column(db.String(50))
-    FName = db.Column(db.String(50))
-    LName = db.Column(db.String(50))
-    HiredDate = db.Column(db.Date)
-    PTO = db.Column(db.Integer)
-    Sick_Days = db.Column(db.Integer)
-    
-    # Relationships with PatientInformation and Employee_schedule
-    patients_in_care = db.relationship('PatientInformation', backref='doctor')
-    schedules = db.relationship('EmployeeSchedule', backref='employee')
+class UserAccount(db.Model):
+    __tablename__ = 'User_Account'
+    User_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Patient_ID = db.Column(db.Integer, db.ForeignKey('new_patient_information.Patient_ID'))
+    Username = db.Column(db.String(50), unique=True, nullable=False)
+    Password_Hash = db.Column(db.String(60), nullable=False)
 
-class EmployeeSchedule(db.Model):
-    __tablename__ = 'Employee_schedule'
-    Employee_ID = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'), primary_key=True)
-    Date = db.Column(db.Date, primary_key=True)
-    Time = db.Column(db.Time, primary_key=True)
+class Appointments(db.Model):
+    __tablename__ = 'Appointments'
+    Appointment_ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Patient_ID = db.Column(db.Integer, db.ForeignKey('new_patient_information.Patient_ID'))
+    Employee_ID = db.Column(db.Integer, db.ForeignKey('employee.Employee_ID'))
+    Appointment_Date = db.Column(db.Date, nullable=False)
+    Appointment_Time = db.Column(db.Time, nullable=False)
+    Type_Of_Visit = db.Column(db.String(50))
 
-class Schedule(db.Model):
-    __tablename__ = 'Schedule'
-    Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient.Patient_ID'), primary_key=True)
-    Employee_ID = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'), primary_key=True)
-    Date = db.Column(db.Date, primary_key=True)
-    Time = db.Column(db.Time, primary_key=True)
-    Type_of_visit = db.Column(db.String(50))
+
+
+
+
+
+
+
+
+# 
+# class PatientInformation(db.Model):
+#     __tablename__ = 'Patient_Information'
+#     Patient_ID = db.Column(db.Integer, primary_key=True)
+#     Height = db.Column(db.Integer)
+#     Weight = db.Column(db.Integer)
+#     Age = db.Column(db.Integer)
+#     Gender = db.Column(db.String(50))
+#     Blood_Pressure = db.Column(db.Integer)
+#     Medication = db.Column(db.Text)
+#     Symptoms = db.Column(db.Text)
+#     Reason_for_visit = db.Column(db.Text)
+#     Last_Reason_for_visit = db.Column(db.Text)
+#     Doctor_in_care = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'))
+#     
+#     # Assuming a one-to-one relationship with Nurse_Form
+#     nurse_form = db.relationship('NurseForm', backref='patient_information', uselist=False)
+# 
+# class NurseForm(db.Model):
+#     __tablename__ = 'Nurse_Form'
+#     Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient_Information.Patient_ID'), primary_key=True)
+#     Height = db.Column(db.Integer)
+#     Weight = db.Column(db.Integer)
+#     Age = db.Column(db.Integer)
+#     Gender = db.Column(db.String(50))
+#     Blood_Pressure = db.Column(db.Integer)
+#     Medication = db.Column(db.Text)
+#     Symptoms = db.Column(db.Text)
+#     Doctor_in_care = db.Column(db.Integer)
+# 
+# class Insurance(db.Model):
+#     __tablename__ = 'Insurance'
+#     Insurance_Name = db.Column(db.Integer, primary_key=True)
+#     Policy_Number = db.Column(db.Integer)
+#     Covered = db.Column(db.Boolean)
+#     Deductible = db.Column(db.Integer)
+#     
+#     # Assuming a one-to-many relationship with Patient
+#     patients = db.relationship('Patient', backref='insurance')
+# 
+# class Patient(db.Model):
+#     __tablename__ = 'Patient'
+#     Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient_Information.Patient_ID'), primary_key=True)
+#     FName = db.Column(db.String(50))
+#     LName = db.Column(db.String(50))
+#     Insurance_Name = db.Column(db.Integer, db.ForeignKey('Insurance.Insurance_Name'))
+#     Covered = db.Column(db.Boolean)
+#     Phone = db.Column(db.Integer)
+#     Address = db.Column(db.String(255))
+#     EContact_Name = db.Column(db.String(50))
+#     EContact_Phone = db.Column(db.Integer)
+# 
+# class Employee(db.Model):
+#     __tablename__ = 'Employee'
+#     Employee_ID = db.Column(db.Integer, primary_key=True)
+#     Position = db.Column(db.String(50))
+#     FName = db.Column(db.String(50))
+#     LName = db.Column(db.String(50))
+#     HiredDate = db.Column(db.Date)
+#     PTO = db.Column(db.Integer)
+#     Sick_Days = db.Column(db.Integer)
+#     
+#     # Relationships with PatientInformation and Employee_schedule
+#     patients_in_care = db.relationship('PatientInformation', backref='doctor')
+#     schedules = db.relationship('EmployeeSchedule', backref='employee')
+# 
+# class EmployeeSchedule(db.Model):
+#     __tablename__ = 'Employee_schedule'
+#     Employee_ID = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'), primary_key=True)
+#     Date = db.Column(db.Date, primary_key=True)
+#     Time = db.Column(db.Time, primary_key=True)
+# 
+# class Schedule(db.Model):
+#     __tablename__ = 'Schedule'
+#     Patient_ID = db.Column(db.Integer, db.ForeignKey('Patient.Patient_ID'), primary_key=True)
+#     Employee_ID = db.Column(db.Integer, db.ForeignKey('Employee.Employee_ID'), primary_key=True)
+#     Date = db.Column(db.Date, primary_key=True)
+#     Time = db.Column(db.Time, primary_key=True)
+#     Type_of_visit = db.Column(db.String(50))
 
 
 
@@ -149,18 +197,35 @@ def edit_patient(patient_id):
 
 
 
-
-
-
-
-
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
 @app.route('/about')
-def hello():
+def about():
     return render_template('about.html')
+
+@app.route('/graphs')
+def graphs():
+    return render_template('graphs.html')
+
+@app.route('/pay')
+def pay():
+    return render_template('pay.html')
+
+@app.route('/appt')
+def appt():
+    return render_template('appt.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
